74f4444d11883154c9176b3e20d4a2a0278f25d4
k <81578246+kaiiy@users.noreply.github.com>
2024-05-17 01:14:36 +0900

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div>
+        <div className="w-32 h-32 bg-red-500"></div>
+        <div className="flex">
+          <div className="w-32 h-32 bg-blue-500">S</div>
+          <div className="w-32 h-32 bg-green-500"></div></div>
+      </div>
+    </div>
+  );
+}
+
+export default App
