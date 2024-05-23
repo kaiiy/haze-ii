98a4558bedb850e8e10b01c387603e66adc33948
kaiiy <81578246+kaiiy@users.noreply.github.com>
2024-05-23 15:56:52 +0900

    Add Link component to Nav for home button

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,5 @@
 import { MdHome } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const ButtonIcon = () => {
   return <MdHome className="w-10 h-10" />;
@@ -6,13 +7,21 @@ const ButtonIcon = () => {
 
 const Nav = () => {
   return (
-    <div className="fixed left-0 top-1/2 transform -translate-y-1/2 bg-white backdrop-blur-md shadow-sm rounded-r-lg p-4 h-1/2 max-h-screen flex flex-col justify-center border-r">
-      <div className="flex flex-col items-center border">
-        {/* https://zenn.dev/catnose99/scraps/a1379d162cb399  */}
-        <span className="transform -rotate-90 mb-2 text-2xl border">
-          Scene 0
+    <div className="fixed left-0 top-1/2 transform -translate-y-1/2 bg-white backdrop-blur-md shadow-sm rounded-r-lg p-4 h-1/2 max-h-screen border-r">
+      <div className="flex flex-col gap-4">
+        <span
+          className="text-3xl inline"
+          style={{
+            writingMode: "vertical-lr",
+            paddingLeft: "0.3rem",
+          }}
+        >
+          SCENE 0
         </span>
-        <ButtonIcon />
+        <hr />
+        <Link to="/">
+          <ButtonIcon />
+        </Link>
       </div>
     </div>
   );
